09204f947eea18de5195bddbe73b9f63c3f2063d
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-08-26 18:56:10 -0300

    add: implement initial tests for page title and slogan visibility in home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -6,3 +6,10 @@ test('deve exibir o slogan na aba do navegador', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Lunar Pass — Passagens para a Lua");
 });
+
+test('deve exibir o slogan na homepage', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const slogan = page.getByRole('heading', { name: 'Sua viagem para a lua começa aqui.' });
+  await expect(slogan).toBeVisible();
+});
